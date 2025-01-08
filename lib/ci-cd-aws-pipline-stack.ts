9c1eb4deb072fd2e65574831fdb4bec816ea711f
rikuteq <elias.irons@edu.sait.ca>
2025-01-07 23:23:59 -0700

    github token

--- lib/ci-cd-aws-pipline-stack.ts
+++ lib/ci-cd-aws-pipline-stack.ts
@@ -9,11 +9,16 @@ export class CiCdAwsPiplineStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
+    //GitHub token from AWS Secrets Manager
+    const githubToken = SecretValue.secretsManager('github-token');
+
     // The code that defines your stack goes here
     new CodePipeline(this, 'CiCdAwsPiplinePipeline', {
       pipelineName: 'CiCdAwsPiplinePipeline',
       synth: new ShellStep('Synth', {
-            input: CodePipelineSource.gitHub('rikuteq/ci-cd-aws-pipline', 'main'),
+            input: CodePipelineSource.gitHub('rikuteq/ci-cd-aws-pipline', 'main', {
+              authentication: githubToken,
+            }),
             commands: [
               'npm ci',
               'npm run build',
